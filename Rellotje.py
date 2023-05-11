25d2c9a1428a0bd1c05c9c338d6ff25fb391caf6
JM <115455650+Jaureo@users.noreply.github.com>
2023-05-11 12:57:04 +0200

    Rellotje.py
    V2

--- Rellotje.py
+++ Rellotje.py
@@ -1,11 +1,11 @@
-#Encender o apagar
+#Encendre o apagar
 Power = 0
 if Power == 0:
     Power = 1
-#Reloj
+#Rellotje
 import time
-Segundos = 0
+Segons = 0
 while Power == 1:
     time.sleep(1)
-    Segundos = Segundos +1
-    print (Segundos)
+    Segundos = Segons +1
+    print (Segons)
